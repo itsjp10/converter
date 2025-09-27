71aca60f995a5b530735c04fae81a6f887fc615e
Jean Pierre <jalvarezidep@gmail.com>
2025-09-27 18:24:44 -0500

    fixed: { id } in the await params added

--- src/app/dashboard/transcriptions/[id]/page.jsx
+++ src/app/dashboard/transcriptions/[id]/page.jsx
@@ -1,8 +1,8 @@
 
 
-export default async function SingleTranscription({ params }){
-    const id = await params;
-    return(
+export default async function SingleTranscription({ params }) {
+    const { id } = await params;
+    return (
         <h1>This is transcription with id: {id}</h1>
     )
 }
